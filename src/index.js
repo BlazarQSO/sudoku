8b55a869094adb72f1fbe1b607e0edff659cd43d
BlazarQSO <BlazarQSO@gmail.com>
2019-10-16 02:31:10 +0300

    sudoku/feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,41 +1,80 @@
 module.exports = function solveSudoku(matrix) {
-    // let n = matrix[0].length;
-	let n = 9;
+    let n = matrix[0].length;
+
     for (let i = 0; i < n; i++) {
-        let row = matrix[i];
-        let colm = [];
-        for (let p = 0; p < n; p++) {
-            colm.push(matrix[p][i]);
-        }
-
-        for (let j = 0; j < n; j++) {                    
-            for (let k = j + 1; k < n; k++) {                        
-                if ((row[j] == row[k] && row[j] != 0 && row[k] != 0) ||
-                    (colm[j] == colm[k] && colm[j] != 0 && colm[k] != 0)) {
-                    return false;
-                }
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            } else {
+                let temp = matrix[i][j];
+                matrix[i][j] = [temp];
             }
-        }
-    }
-
-    let region = [];
-    for (let k1 = 0; k1 < n / 3; k1++) {
-        for (let k2 = 0; k2 < n / 3; k2++) {
-            for (let i = k1 * 3; i < k1 * 3 + 3; i++) {
-                for (let j = k2 * 3; j < k2 * 3 + 3; j++) {
-                    region.push(matrix[i][j]);
-                }
-            }
-            for (let t = 0; t < n; t++) {
-                for (let s = t + 1; s < n; s++) {
-                    if (region[t] == region[s] && region[s] != 0) {
-                        return false;
-                    }
-                }
-            }
-            region = [];
         }
     }
     
-    return true;
+    while (matrix.join("").length > 153) {
+
+        let region = new Array(3);
+        for (let q = 0; q < region.length; q++) {
+            region[q] = new Array(3);
+        }
+        for (let k1 = 0; k1 < 3; k1++) {
+            for (let k2 = 0; k2 < 3; k2++) {
+                let item = "";
+                for (let i = k1 * 3; i < k1 * 3 + 3; i++) {
+                    for (let j = k2 * 3; j < k2 * 3 + 3; j++) {
+                        if (matrix[i][j].length == 1) {
+                            item += matrix[i][j][0];
+                        }
+                    }
+                }
+                region[k1][k2] = item;
+            }
+        }
+
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+
+                if (matrix[i][j].length > 1) {
+
+                    let row = "";
+                    for (let r = 0; r < n; r++) {
+                        if (matrix[i][r].length == 1) {
+                            row += matrix[i][r][0];
+                        }
+                    }
+
+                    let colm = "";
+                    for (let c = 0; c < n; c++) {
+                        if (matrix[c][j].length == 1) {
+                            colm += matrix[c][j][0];
+                        }
+                    }
+
+                    let x = Math.trunc(j / 3);
+                    let y = Math.trunc(i / 3);
+                    let allNumbers = region[y][x] + row + colm;
+
+                    for (let d = 0; d < allNumbers.length; d++) {
+                        if (matrix[i][j].toString().includes(allNumbers[d])) {
+                            let index = matrix[i][j].indexOf(Number(allNumbers[d]));
+                            matrix[i][j].splice(index, 1);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    let matrix2 = new Array(9);
+    for (let q = 0; q < region.length; q++) {
+         matrix2[q] = new Array(9);
+    }
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            matrix2[i][j] = matrix[i][j][0];
+        }
+    }
+
+    return matrix2;
 }
